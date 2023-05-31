3b820127f543fa540a91a4fc384ebfbd1597c632
anna <anna.riazantseva123321@gmail.com>
2023-05-31 11:50:21 +0300

    version 4

--- index.js
+++ index.js
@@ -9,17 +9,18 @@ function MyArray(...args) {
  function MyArrayProto(){
     this.every = function(fn){
         if(this.length === 0){
-            return false;
+            return true;
         }
         for(let i = 0; i < this.length; i++){
             if(fn(this[i], i, this)){
                 return true;
             }
         }
+        return false
     } 
  }
    
-const myArray = [2, 4, 6, 12];
+const myArray = [];
 console.log(myArray.every(el => el % 2 === 0))
 const myArray2 = [3, 4, 6, 12];
 console.log(myArray2.every(el => el % 2 === 0))
